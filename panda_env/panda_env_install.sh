9dcd248395db0d30d3b85f73187f3aed6be7ebd9
Wen Guan <wguan.icedew@gmail.com>
2024-01-19 17:03:47 +0100

    set default panda to usdf and use new bps

--- panda_env/panda_env_install.sh
+++ panda_env/panda_env_install.sh
@@ -185,22 +185,25 @@ function install_panda_setup () {
     cat <<- EOF > $rootDir/setup_panda.sh
 #!/bin/bash
 
-echo "Setup BPS DOMA PanDA (at CERN) environment"
+echo "Setup BPS USDF PanDA (at SLAC K8S) environment"
+
 # setup PanDA env. Will be a simple step when the deployment of PanDA is fully done.
 export PANDA_CONFIG_ROOT=\$HOME/.panda
-export PANDA_URL_SSL=https://pandaserver-doma.cern.ch:25443/server/panda
-export PANDA_URL=http://pandaserver-doma.cern.ch:25080/server/panda
+export PANDA_URL_SSL=https://usdf-panda-server.slac.stanford.edu:8443/server/panda
+export PANDA_URL=https://usdf-panda-server.slac.stanford.edu:8443/server/panda
 export PANDACACHE_URL=\$PANDA_URL_SSL
-export PANDAMON_URL=https://panda-doma.cern.ch
+export PANDAMON_URL=https://usdf-panda-bigmon.slac.stanford.edu:8443/
 export PANDA_AUTH=oidc
 export PANDA_VERIFY_HOST=off
 export PANDA_AUTH_VO=Rubin
+
+export PANDA_BEHIND_REAL_LB=true
 
 # IDDS_CONFIG path depends on the weekly version
 export PANDA_SYS=\$CONDA_PREFIX
 export IDDS_CONFIG=\${PANDA_SYS}/etc/idds/idds.cfg.client.template
 
-export IDDS_MAX_NAME_LENGTH=4000
+export IDDS_MAX_NAME_LENGTH=30000
 
 # WMS plugin
 export BPS_WMS_SERVICE_CLASS=lsst.ctrl.bps.panda.PanDAService
